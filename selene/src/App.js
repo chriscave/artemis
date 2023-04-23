8207412a4e1ec42fd54eea76afc6cfb322c30d18
Chris <chriscave89@gmail.com>
2023-04-23 08:05:24 +0200

    remove comments

--- selene/src/App.js
+++ selene/src/App.js
@@ -9,24 +9,4 @@ function App() {
   );
 }
 
-// React component
-// import React, { useState } from "react";
-
-// function App() {
-//   const [message, setMessage] = useState("");
-
-//   const handleClick = async () => {
-//     const response = await fetch("api/hello");
-//     const data = await response.json();
-//     setMessage(data.message);
-//   };
-
-//   return (
-//     <div>
-//       <button onClick={handleClick}>Say Hello</button>
-//       <p>{message}</p>
-//     </div>
-//   );
-// }
-
 export default App;
